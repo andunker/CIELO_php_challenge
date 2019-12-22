7dc0e20ad4f1e3e0e01563bd1822222a9c4f616e
Fabio Mora <andunker@hotmail.com>
2019-12-22 13:44:32 -0500

    default home view and jquery

---
+++ application/views/pages/home.php
@@ -0,0 +1,34 @@
+  
+  <div class="container">
+      <div class="row">
+          <div class="col-md-4">
+              <h3>Column 1</h3>
+              
+              <form action="/action_page.php"> 
+
+              <div class="form-group">
+                  <label for="name">Name:</label>
+                  <input type="text" class="form-control" id="name">
+              </div>
+
+              <div class="form-group">
+                  <label for="birth">Date of birthday:</label>
+                  <input type="text" class="form-control" id="birth">
+              </div>
+
+              <div class="form-group">
+                  <label for="email">Email:</label>
+                  <input type="text" class="form-control" id="email">
+              </div>
+
+              <div class="form-group">
+                  <label for="color">Favorite color:</label>
+                  <input type="text" class="form-control" id="color">
+              </div>
+
+              <input type="submit" class="btn btn-primary" name="submit" value="Create" />
+
+              </form>
+          </div>
+      </div>
+  </div>
--- application/views/templates/header.php
+++ application/views/templates/header.php
@@ -2,10 +2,11 @@
 <html lang="en">
 
 <head>
-        <title>CodeIgniter Tutorial</title>
+        <title>CodeIgniter</title>
         <meta charset="utf-8">
 
         <link rel="stylesheet" href="vendor/twbs/bootstrap/dist/css/bootstrap.min.css">
+        <script src="vendor/components/jquery/jquery.min.js"></script>
         <script src="vendor/twbs/bootstrap/dist/js/bootstrap.min.js"></script>
 </head>
 
